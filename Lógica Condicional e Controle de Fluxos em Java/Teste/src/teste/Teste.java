690b6d3184f14c748d2284e0f4e53f9cf0324890
igorghirardelli <igorghirardelli@gmail.com>
2022-11-03 18:42:57 -0300

    /Lógica Condicional e Controle de Fluxos em Java

---
+++ Lógica Condicional e Controle de Fluxos em Java/Teste/src/teste/Teste.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste;
+
+/**
+ *
+ * @author igora
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // teste só parar upar para o github
+        System.out.println("OLA AO CURSO!");
+    }
+    
+}
