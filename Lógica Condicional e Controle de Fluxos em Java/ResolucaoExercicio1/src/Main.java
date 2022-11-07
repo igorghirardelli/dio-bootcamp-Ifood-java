bbbe7b62626db060dd8f60985c0ff9a33405330c
igorghirardelli <igorghirardelli@gmail.com>
2022-11-07 19:33:28 -0300

    Lógica Condicional e Controle de Fluxos em Java

---
+++ Lógica Condicional e Controle de Fluxos em Java/ResolucaoExercicio1/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        int i1 = 10;
+        int in2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        String s3 = "Cicrano";
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597L;
+        long l2 = 8997L;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("l1 == l2"+(l1 == l2)); 
+        System.out.println("l1 != l2"+(l1 != l2));
+        System.out.println("l1 > l2"+(l1 > l2));
+        System.out.println("l1 <= l2"+(l1 <= l2));
+
+        System.out.println("f1 == f2"+(f1 == f2));
+        System.out.println("f1 != f2"+(f1 != f2));
+        System.out.println("f1 > f2"+(f1 > f2));
+        System.out.println("f1 <= f2"+(f1 <= f2));
+
+        System.out.println("c1 == c2"+(c1 == c2));
+        System.out.println("c1 != c2"+(c1 != c2));
+        System.out.println("c1 > c2"+(c1 > c2));
+        System.out.println("c1 <= c2"+(c1 <= c2));
+
+
+
+
+
+
+
+
+    }
+}
